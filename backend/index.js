7440444aa5ddd958bf601805345a5ea8d176bf31
nikhilrana13 <nikhilrajput060@gmail.com>
2025-12-31 14:25:16 +0530

    chore: initialize backend package with dependencies and connect to db

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000 
+const app = express()
+
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+
+
+// Routes 
+
+
+
+
+// connect to database 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to db")
+}).catch((error)=>{
+    console.log("failed to connect to db",error)
+})
+
+// app.use("/",(req,res)=>{
+//     res.send("server is running")
+// })
+
+app.listen(PORT,(()=>{
+    console.log(`Server is running on ${PORT}`)
+}))
