86c5de7e7f030e0e44b032da73d42cf0c240535c
nikhilrana13 <nikhilrajput060@gmail.com>
2026-01-07 19:08:37 +0530

    Updated mcadmin layout and sidebar components

--- frontend/src/app/mcadmin/layout.js
+++ frontend/src/app/mcadmin/layout.js
@@ -5,11 +5,11 @@ const Mclayout = ({children}) => {
   return (
      <div className='w-full flex flex-col agp-1 md:flex-row '>
         {/* left side */}
-        <div className='w-full md:w-[20%] border min-h-screen'>
+        <div className='w-full md:w-[20%] border-r border-gray-300 min-h-screen'>
             <Sidebar />
         </div>
         {/* right side  */}
-        <div className='w-full md:w-[80%] flex flex-col border'>
+        <div className='w-full md:w-[80%] flex flex-col '>
             {children}
         </div>
     </div>
--- frontend/src/components/commen/Sidebar.jsx
+++ frontend/src/components/commen/Sidebar.jsx
@@ -1,8 +1,61 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { IoIosNotifications, IoIosSettings } from 'react-icons/io'
+import { IoLogOut } from 'react-icons/io5'
+import { LuLayoutDashboard } from 'react-icons/lu'
+import { RiGovernmentLine } from 'react-icons/ri'
+import { TbNotes } from 'react-icons/tb'
 
 const Sidebar = () => {
+  const pathname = usePathname()
+
+  const navlink = (path)=>{
+           return `${pathname === path ? "bg-[#0A3D62] text-white px-3 py-3  rounded-md  flex items-center gap-2" : "px-3 py-3  hover:text-[#0A3D62] text-[#707785] hover:bg-[#E9EEF2] rounded-md "}`
+  }
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col min-h-screen  bg-[#F0F4F8]'>
+      <div className='flex items-center border-b border-gray-300 py-3 px-4 gap-3'>
+         <div className='px-3 py-3 rounded-full bg-[#18486B] ' >
+                            <RiGovernmentLine size={25} className=' text-white' />
+          </div>
+           <span className='text-[#18486B] font-semibold text-[0.8rem] md:text-[1.1rem]'>MCC Delhi</span>
+      </div>
+      <div className='flex flex-col pt-8 px-4  gap-5'>
+        <Link href="/mcadmin/dashboard" className={navlink("/mcadmin/dashboard")}>
+              <div className="flex items-center gap-4">
+                <LuLayoutDashboard size={25} />
+                <span className="transition-opacity font-medium duration-500">Dashboard</span>
+                  
+              </div>
+          </Link>
+          <Link href="/mcadmin/complaints" className={navlink("/mcadmin/complaints")}>
+              <div className="flex items-center gap-4">
+                 <TbNotes size={25} />
+                <span className="transition-opacity font-medium duration-500">Complaints</span>
+              </div>
+          </Link>
+          <Link href="/mcadmin/notification" className={navlink("/mcadmin/notification")}>
+              <div className="flex items-center gap-4">
+                <IoIosNotifications size={25} />
+                <span className="transition-opacity font-medium duration-500">Notifications</span>
+              </div>
+          </Link>
+          <Link href="/mcadmin/settings" className={navlink("/mcadmin/settings")}>
+              <div className="flex items-center gap-4">
+                <IoIosSettings size={25} />
+                <span className="transition-opacity font-medium duration-500">Settings</span>
+              </div>
+          </Link>
+      </div>
+       <div className='border-t  border-gray-300 px-6 cursor-pointer py-5 mt-65'>
+         <span className='flex hover:text-[#0A3D62] rounded-md cursor-pointer px-3 py-3 text-[#707785] hover:bg-[#E9EEF2] gap-2'>
+         <IoLogOut size={25} />
+            <span className='text-'>Logout</span>
+         </span>
+       </div>
+    </div>
   )
 }
 
